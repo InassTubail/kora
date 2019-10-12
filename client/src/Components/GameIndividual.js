7cfb50cb0ae7e8101e7894db33bb3c6f7442d59a
samarFayez <s.aljazzar@unitone.co>
2019-10-12 14:08:56 +0300

    individual

--- client/src/Components/GameIndividual.js
+++ client/src/Components/GameIndividual.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import koraImg from '../assets/kora.png';
-import person from '../assets/1.png';
+// import person from '../assets/1.png';
+import { person } from './playersImage';
 import frame from '../assets/frame.png';
 import hares from '../assets/hares.png';
 import titleImg from '../assets/tit.png';
@@ -23,79 +24,85 @@ function shuffle(array) {
 
   return array;
 }
+function questionsAndAnswers(level) {
+  var number1, number2, answers = [];
+  switch (level) {
+    case 1:
+      number1 = Math.floor(Math.random() * 3)
+      number2 = Math.floor(Math.random() * 12) + 1
+      break;
+    case 2:
+      number1 = Math.floor(Math.random() * 6) + 4
+      number2 = Math.floor(Math.random() * 12)
+      break;
+    case 3:
+      number1 = Math.floor(Math.random() * 9) + 7;
+      number2 = Math.floor(Math.random() * 12)
+      break;
+    case 4:
+      number1 = Math.floor(Math.random() * 12) + 10;
+      number2 = Math.floor(Math.random() * 12)
+      break;
+    default:
+  }
+  if (number1 || number2) {
+    answers.push({ answer: number1 * number2, style: "correct" })
+    while (answers.length !== 3) {
+      let randomly = ({ answer: (number1 * number2) + Math.floor(Math.random() * 12), style: "incorrect" })
+      if (!answers.every((el)=>el.answer === randomly.answer)) {
+        answers.push(randomly)
+      }
+    }
+    answers = shuffle(answers)
+  }
+  return { number1, number2, answers }
+}
 class GameIndividual extends Component {
-  // const GameIndividual = () => {
-  // state = { addClass: false };
-
-  // toggle() {
-  //   console.log('hhh');
-  //   this.setState({ addClass: true });
-  // }
   state = {
     number1: 0,
     number2: 0,
-    answer: [],
+    answers: [],
+    person: "",
     NOTrue: 0,
+    isClick: false,
     NOQuestion: 0, //when on Click it must be +1 when become 6 appear popup
   }
   // did upfate for changing level next props !== next state
-  componentDidUpdate(){
-    const { NOQuestion,NOTrue } = this.state;
+  componentDidUpdate() {
+    const { NOQuestion, NOTrue, isClick } = this.state;
     if (NOQuestion === 6) {
       if (NOTrue >= 3) {
         //popup sucess
         // chamge level to +1
-      }else{
+      } else {
         //popup again game
       }
     }
+    if (isClick) {
+      setTimeout(async () => {
+        this.setState({ isClick: false })
+        const { number1, number2, answers } = questionsAndAnswers(this.props.user_info.level);
+        this.setState({ number1, number2, answers })
+      }, 2000);
+    }
   }
   componentDidMount() {
-    const { score, level } = this.props.user_info
-    switch (level) {
-      case 1:
-        this.setState({
-          number1: Math.floor(Math.random() * 3), number2: Math.floor(Math.random() * 12) + 1
-        })
-        break;
-      case 2:
-        this.setState({
-          number1: Math.floor(Math.random() * 6) + 4, number2: Math.floor(Math.random() * 12)
-        })
-        break;
-      case 3:
-        this.setState({
-          number1: Math.floor(Math.random() * 9) + 7, number2: Math.floor(Math.random() * 12)
-        })
-        break;
-      case 4:
-        this.setState({
-          number1: Math.floor(Math.random() * 12) + 10, number2: Math.floor(Math.random() * 12)
-        })
-        break;
-      default:
-      // code block
+    const { number1, number2, answers } = questionsAndAnswers(this.props.user_info.level);
+    this.setState({ number1, number2, answers })
+
+  }
+  selectAnswer = (el) => {
+    const { id } = el.currentTarget
+    const { number2, number1, NOQuestion, NOTrue } = this.state;
+    this.setState({ isClick: true })
+    if (number2 * number1 == id) {
+      this.setState({ NOQuestion: NOQuestion + 1, NOTrue: NOTrue + 1 })
+    } else {
+      this.setState({ NOQuestion: NOQuestion + 1 })
     }
   }
   render() {
-    // const answers = ['answer1'];
-    // if (this.state.addClass) {
-    //   answers.push('green');
-    // }
-
-    const { number1, number2 } = this.state;
-    var answers = []
-    if (number1 || number2) {
-      answers.push(this.state.number1 * this.state.number2)
-      while (answers.length !== 3) {
-        let randomly = ((this.state.number1 * this.state.number2) + Math.floor(Math.random() * 12))
-        if (!answers.includes(randomly)) {
-          answers.push(randomly)
-        }
-      }
-      answers = shuffle(answers);
-    }
-    console.log({ answers });
+    const { number1, number2, answers } = this.state;
     return (
       <div className="gameScreen">
         <div className="header">
@@ -108,10 +115,10 @@ class GameIndividual extends Component {
         <img src={hares} alt="hares" className="hares" />
 
         <div className="answers">
-          {answers.map((el,index)=> 
-          <div className={`answer${index+1}`}>
-            <p>{el}</p>
-          </div>
+          {answers.map((el, index) =>
+            <button className={!this.state.isClick ? `answer${index + 1}` : `answer${index + 1} ${el.style}`} id={el.answer} onClick={this.selectAnswer}>
+              {el.answer}
+            </button>
           )}
         </div>
         <img src={koraImg} alt="kora" edt className="koraImg" />
@@ -124,7 +131,7 @@ class GameIndividual extends Component {
             alt="dss"
             className="selectedImageFrame33"
           />
-          <img src={person} title="sdd" alt="dd" className="selectedImage33" />
+          <img src={person(this.props.user_info.person)} title="sdd" alt="dd" className="selectedImage33" />
         </div>
         <div className="subHeader4">
           <img src={counter} title="sdd" alt="dd" className="counter" />
