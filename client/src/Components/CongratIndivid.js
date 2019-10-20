2f569d48335fb1b196ccddee898177261bd8b5e8
samarFayez <s.aljazzar@unitone.co>
2019-10-20 15:55:52 +0300

    some update

--- client/src/Components/CongratIndivid.js
+++ client/src/Components/CongratIndivid.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import playAgain from '../assets/playAgain.png'
 import { connect } from 'react-redux';
+import { updateUser } from '../store/actions';
+
 import './CongratIndivid.css';
 
 class GameIndividual extends Component {
+  replay = () => {
+    this.props.updateUser({ ...this.props.user_info, level: 1 })
+    this.props.history.push(`/select-game-type`);
+  }
   render() {
     return (
       <React.Fragment>
@@ -13,9 +19,9 @@ class GameIndividual extends Component {
             <p className="winnerPlayer">{this.props.user_info.username}</p>
           </div>
           <div className="playAgain">
-        <Link>
-<img src={playAgain} alt="playAgain"/>
-</Link>
+            <Link onClick={this.replay}>
+              <img src={playAgain} alt="playAgain" />
+            </Link>
           </div>
         </div>
       </React.Fragment>
@@ -23,9 +29,11 @@ class GameIndividual extends Component {
   }
 };
 
+const mapDispatchToProps = { updateUser };
 const mapStateToProps = state => ({
   user_info: state.user.info,
 });
 export default connect(
   mapStateToProps,
+  mapDispatchToProps
 )(GameIndividual);
