59ca9293d3ffff6769b72844d1d4118e7f7c4c9e
samarFayez <s.aljazzar@unitone.co>
2019-10-21 21:24:18 +0300

    sa

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,12 @@
 const io = require('socket.io-client');
-let port = process.env.PORT || 'http://localhost:8080' 
+let port = process.env.PORT 
+// || 'http://localhost:8080' 
+console.log(process.env,'PORTPORT');
+console.log(process.env.PORT,'PORT');
+
+console.log({port});
+
 const socket = io.connect(port);
+console.log({socket});
+
 export default socket; 
