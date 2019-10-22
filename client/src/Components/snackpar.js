af989bbb040aa5cbaeb3089da0a13421225e69bf
samarFayez <s.aljazzar@unitone.co>
2019-10-22 21:21:03 +0300

    sa

--- client/src/Components/snackpar.js
+++ client/src/Components/snackpar.js
@@ -3,21 +3,17 @@ import './snackpar.css'
 
 function Snackbar(propss) {
     const { props } = propss;
+    console.log(props.open,'props');
+    
     return props.open &&
-        (props.type === 'cancelInvite' ||
-            props.type === 'cancelPlayer' ||
-            props.type === 'withdrawal' ||
-            props.type === 'reject'
-        ) ? (
-            <div id="snackbar" className="show" >
-                {props.type === 'cancelPlayer' && props.type === 'cancelInvite' && <p>
-                    ثم استثناءك من اللعبة من قبل {props.come_from}
-                </p>
-                }
-                {props.type === 'withdrawal' && <p>  {props.come_from}من اللعبه انسحب </p>}
-                {props.type === 'reject' && <p>  {props.come_from} تم رفض الدعوة من قبل  </p>}
+        <div id="snackbar" >
+            {(props.type === 'cancelPlayer' || props.type === 'cancelInvite') && <p>
+                ثم استثناءك من اللعبة من قبل {props.come_from}
+            </p>
+            }
+            {props.type === 'withdrawal' && <p>  {props.come_from}من اللعبه انسحب </p>}
+            {props.type === 'reject' && <p>  {props.come_from} تم رفض الدعوة من قبل  </p>}
 
-            </div>
-        ) : null;
+        </div>
 }
 export default Snackbar;
