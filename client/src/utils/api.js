7c7fbbcf2b27a2edcfff731ecd4e6d40244a8dbd
Samer El-Aila <samerelaila0@gmail.com>
2019-10-24 20:43:00 +0300

    fix connection url which causing deploy issues

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,13 +1,16 @@
 const io = require('socket.io-client');
-let port = process.env.PORT 
-// || 'http://localhost:8080' 
-console.log(process.env,'PORTPORT');
-console.log(process.env.PORT,'PORT');
+let port = process.env.PORT;
+// || 'http://localhost:8080'
+console.log(process.env, 'PORTPORT');
+console.log(process.env.PORT, 'PORT');
 
-console.log({port});
+console.log({ port });
 
-const socket = io.connect('http://localhost:8080');
-console.log({socket});
+console.log({ nodeEnd: process.env.NODE_ENV });
+let connectionUrl =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : undefined;
 
+const socket = io.connect(connectionUrl);
+console.log({ socket });
 
-export default socket; 
+export default socket;
